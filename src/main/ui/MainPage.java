1106519ce71e25e966bafa996d00e287ff8efe15
Isabella Han <1826291293@qq.com>
2022-03-22 10:26:07 -0700

    Model

--- src/main/ui/MainPage.java
+++ src/main/ui/MainPage.java
@@ -1,13 +1,15 @@
 package ui;
 
+import model.TodayDiary;
 import org.junit.jupiter.api.AfterAll;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.List;
 
 public class MainPage extends JFrame {
     JButton bar;
-    JTextField diaryField;
+    JLabel text;
     JButton save;
     JButton add;
     JButton quit;
@@ -22,10 +24,13 @@ public class MainPage extends JFrame {
 
         JPanel p = new JPanel();
         this.setContentPane(p);
+        p.setOpaque(false);
+        p.setLayout(null);
 
 
         save = new JButton("Save");
         save.setFont(new Font("Bond", Font.BOLD, 30));
+        save.setBounds(190,0,100,60);
         save.setBorderPainted(true);
         save.setBackground(Color.GREEN);
         save.setForeground(Color.BLACK);
@@ -34,6 +39,7 @@ public class MainPage extends JFrame {
 
         add = new JButton("+");
         add.setFont(new Font("Bond", Font.BOLD, 30));
+        add.setBounds(310,0,100,60);
         add.setBorderPainted(true);
         add.setBackground(Color.PINK);
         add.setForeground(Color.BLACK);
@@ -42,20 +48,30 @@ public class MainPage extends JFrame {
 
         quit = new JButton("Quit");
         quit.setFont(new Font("Bond", Font.BOLD, 30));
+        quit.setBounds(430, 0,100,60);
         quit.setBorderPainted(true);
         quit.setBackground(Color.RED);
         quit.setForeground(Color.BLACK);
         quit.setOpaque(true);
         p.add(quit);
-        p.setOpaque(false);
 
         bar = new JButton("Column Chart [HAPPY * SAD * ANGRY * CALM]");
         bar.setFont(new Font("Bond", Font.BOLD, 30));
+        bar.setBounds(20, 70, 760,60);
         bar.setBorderPainted(true);
-        bar.setBackground(Color.GREEN);
+        bar.setBackground(Color.BLUE);
         bar.setForeground(Color.BLACK);
-        bar.setOpaque(false);
+        bar.setOpaque(true);
         p.add(bar);
+
+        String str = "UMMMMM";
+        text = new JLabel(str);
+        text.setFont(new Font("Bond", Font.BOLD, 20));
+        text.setBounds(0,150,800,700);
+        text.setBackground(Color.BLACK);
+        text.setForeground(Color.WHITE);
+        text.setOpaque(true);
+        p.add(text);
 
     }
 }
