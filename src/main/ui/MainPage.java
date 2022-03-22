fa11901ff1636a99f9a741eea20619465d96f7f5
Isabella Han <1826291293@qq.com>
2022-03-22 11:11:08 -0700

    Model

--- src/main/ui/MainPage.java
+++ src/main/ui/MainPage.java
@@ -1,10 +1,13 @@
 package ui;
 
+import model.PreviousDiary;
 import model.TodayDiary;
-import org.junit.jupiter.api.AfterAll;
+import persistance.JsonReader;
+import persistance.JsonWriter;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 import java.util.List;
 
 public class MainPage extends JFrame {
@@ -13,9 +16,19 @@ public class MainPage extends JFrame {
     JButton save;
     JButton add;
     JButton quit;
+    private PreviousDiary previousDiary;
+    private JsonWriter jsonWriter;
+    private JsonReader jsonReader;
+    private static final String JSON_STONE = "./data/todaydiary.txt";
+
 
     public MainPage() {
         super("");
+
+        previousDiary = new PreviousDiary("My Diaries");
+        jsonWriter = new JsonWriter(JSON_STONE);
+        jsonReader = new JsonReader(JSON_STONE);
+
         this.setSize(800, 800);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
@@ -30,7 +43,7 @@ public class MainPage extends JFrame {
 
         save = new JButton("Save");
         save.setFont(new Font("Bond", Font.BOLD, 30));
-        save.setBounds(190,0,100,60);
+        save.setBounds(190, 0, 100, 60);
         save.setBorderPainted(true);
         save.setBackground(Color.GREEN);
         save.setForeground(Color.BLACK);
@@ -39,7 +52,7 @@ public class MainPage extends JFrame {
 
         add = new JButton("+");
         add.setFont(new Font("Bond", Font.BOLD, 30));
-        add.setBounds(310,0,100,60);
+        add.setBounds(310, 0, 100, 60);
         add.setBorderPainted(true);
         add.setBackground(Color.PINK);
         add.setForeground(Color.BLACK);
@@ -48,7 +61,7 @@ public class MainPage extends JFrame {
 
         quit = new JButton("Quit");
         quit.setFont(new Font("Bond", Font.BOLD, 30));
-        quit.setBounds(430, 0,100,60);
+        quit.setBounds(430, 0, 100, 60);
         quit.setBorderPainted(true);
         quit.setBackground(Color.RED);
         quit.setForeground(Color.BLACK);
@@ -57,21 +70,41 @@ public class MainPage extends JFrame {
 
         bar = new JButton("Column Chart [HAPPY * SAD * ANGRY * CALM]");
         bar.setFont(new Font("Bond", Font.BOLD, 30));
-        bar.setBounds(20, 70, 760,60);
+        bar.setBounds(20, 70, 760, 60);
         bar.setBorderPainted(true);
         bar.setBackground(Color.BLUE);
         bar.setForeground(Color.BLACK);
         bar.setOpaque(true);
         p.add(bar);
 
-        String str = "UMMMMM";
+        loadDiaries();
+
+        String str = printDiaries();
         text = new JLabel(str);
         text.setFont(new Font("Bond", Font.BOLD, 20));
-        text.setBounds(0,150,800,700);
+        text.setBounds(0, 150, 800, 700);
         text.setBackground(Color.BLACK);
         text.setForeground(Color.WHITE);
         text.setOpaque(true);
         p.add(text);
 
+    }
+
+    private String printDiaries() {
+        List<TodayDiary> diaries = previousDiary.getPreviousDiary();
+        String tx = "";
+        for (TodayDiary td : diaries) {
+            tx = tx + td.toString() + "\r\n";
+        }
+        return tx;
+    }
+
+    private void loadDiaries() {
+        try {
+            previousDiary = jsonReader.read();
+        } catch (IOException e) {
+            System.out.println("Unable to read from file: " + JSON_STONE);
+
+        }
     }
 }
